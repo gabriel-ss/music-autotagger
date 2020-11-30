7bc083a18551dd22a0540aeb4ce099941b55f0ac
Gabriel Silveira <gabriel.silveira@poli.ufrj.br>
2020-11-30 19:16:27 -0300

    Add options dictionary

---
+++ src/options.jl
@@ -0,0 +1,7 @@
+options = Dict(
+	"logfile" => "log.txt",
+	"tagsfile" => "MagnaTagATune/annotations_final.csv",
+	"samplespath" => "MagnaTagATune/mp3/",
+	"pathcolumn" => "mp3_path",
+	"excludedcolumns" => ["clip_id"],
+)
